2e0229912f4bf965fcbb5698f6420014654a524a
ayutaz <ka1357amnbpdr@gmail.com>
2019-11-16 00:26:44 +0900

    変数のテスト

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
 class Clear{
+    playerX: number
+    playerY: number
+    playerZ: number
     public getPosition() {
-        player.say("second hello")
+        this.playerX = player.position().getValue(0)
+        this.playerY = player.position().getValue(1)
+        this.playerZ = player.position().getValue(2)
+        player.say(this.playerX.toString() + ",")
     }
 } 
