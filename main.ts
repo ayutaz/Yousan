1c9148fd04b8f228715adf20a60440ea625923bb
ayutaz <ka1357amnbpdr@gmail.com>
2019-11-15 22:19:04 +0900

    [編集] getPositionの前にpublicをつけた

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 class Clear{
     // 自分の現在の位置を取得
-getPosition() {
+    public getPosition() {
     // getValueは配列指定 0 -> x,1 -> y...
-    playerX = player.position().getValue(0)
-    playerY = player.position().getValue(1)
-    playerZ = player.position().getValue(2)
-    player.say("player position:(" + playerX.toString() + "," + playerY.toString() + "," + playerZ.toString() + ")")
-    playerY = positions.groundPosition(player.position()).getValue(1)
-    player.say("地面の座標:" + playerY)
-}
+        playerX = player.position().getValue(0)
+        playerY = player.position().getValue(1)
+        playerZ = player.position().getValue(2)
+        player.say("player position:(" + playerX.toString() + "," + playerY.toString() + "," + playerZ.toString() + ")")
+        playerY = positions.groundPosition(player.position()).getValue(1)
+        player.say("地面の座標:" + playerY)
+    }
 } 
