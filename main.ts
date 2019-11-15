89b5462cf05626b7120ced629313e37971f5d4f7
ayutaz <ka1357amnbpdr@gmail.com>
2019-11-16 00:48:47 +0900

    変数の前にthisの追加

--- main.ts
+++ main.ts
@@ -9,12 +9,15 @@ class Clear{
     }
     public getPosition() {
         // getValueは配列指定 0 -> x,1 -> y...
-        playerX = player.position().getValue(0)
-        playerY = player.position().getValue(1)
-        playerZ = player.position().getValue(2)
-        player.say("player position:(" + playerX.toString() + "," + playerY.toString() + "," + playerZ.toString() + ")")
-        playerY = positions.groundPosition(player.position()).getValue(1)
-        player.say("地面の座標:" + playerY)
+        this.playerX = player.position().getValue(0)
+        this.playerY = player.position().getValue(1)
+        this.playerZ = player.position().getValue(2)
+        player.say("player position:(" +
+            this.playerX.toString() + "," +
+            this.playerY.toString() + "," +
+            this.playerZ.toString() + ")")
+        this.playerY = positions.groundPosition(player.position()).getValue(1)
+        player.say("地面の座標:" + this.playerY)
     }
     public call() {
         player.say(this.playerX.toString() + "," + this.playerY.toString())
