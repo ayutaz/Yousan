94214fd8a0d0e5c31bf65c18440c72d561d4bfef
ayutaz <ka1357amnbpdr@gmail.com>
2019-11-16 00:29:20 +0900

    getPositionの追加

--- main.ts
+++ main.ts
@@ -3,9 +3,12 @@ class Clear{
     playerY: number
     playerZ: number
     public getPosition() {
-        this.playerX = player.position().getValue(0)
-        this.playerY = player.position().getValue(1)
-        this.playerZ = player.position().getValue(2)
-        player.say(this.playerX.toString() + ",")
+        // getValueは配列指定 0 -> x,1 -> y...
+        playerX = player.position().getValue(0)
+        playerY = player.position().getValue(1)
+        playerZ = player.position().getValue(2)
+        player.say("player position:(" + playerX.toString() + "," + playerY.toString() + "," + playerZ.toString() + ")")
+        playerY = positions.groundPosition(player.position()).getValue(1)
+        player.say("地面の座標:" + playerY)
     }
 } 
