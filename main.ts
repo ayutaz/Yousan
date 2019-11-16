4f75b053aa721b57f8d3189959e10f5efc03367a
ayutaz <ka1357amnbpdr@gmail.com>
2019-11-16 12:50:36 +0900

    set関数定義

--- main.ts
+++ main.ts
@@ -1,14 +1,24 @@
 class Clear{
-    playerX: number
-    playerY: number
-    playerZ: number
-    groundPositionY: number
-    deleteRange: number
-    deleteHeight: number
+    // プレイヤーの位置を保持する
+    private playerX: number
+    private playerY: number
+    private playerZ: number
+    private groundPositionY: number
+    // 整地するための範囲変数
+    private deleteRange: number
+    private deleteHeight: number
+
     constructor() {
         this.playerX = 0
         this.playerY = 0
         this.playerZ = 0
+        this.groundPositionY = 0
+        this.deleteHeight = 0
+        this.deleteRange = 0
+    }
+    public SetDeleteNumber(r: number, h: number) {
+        this.deleteHeight = h
+        this.deleteRange = r
     }
     public getPosition() {
         // getValueは配列指定 0 -> x,1 -> y...
